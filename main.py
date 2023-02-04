4d6da0617306a1592963df3510fb5c0ed1b98b8d
B52 <djon9920@gmail.com>
2023-02-04 19:22:53 +0000

    Update ProjectPage

---
+++ main.py
@@ -0,0 +1,55 @@
+import datetime
+
+import flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from typing import Callable
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///admin.db'
+app.config['SECRET_KEY'] = 'adminkey'
+
+Bootstrap(app)
+
+
+class MySQLAlchemy(SQLAlchemy):
+    Column: Callable
+    Integer: Callable
+    String: Callable
+
+
+db = MySQLAlchemy(app)
+
+admin = Admin(app)
+
+
+class Owner(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+
+
+admin.add_view(ModelView(Owner, db.session))
+
+db.create_all()
+
+dat = datetime.datetime.now()
+year = dat.year
+
+
+@app.route("/")
+def home():
+    return render_template("index.html", year=year)
+
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
